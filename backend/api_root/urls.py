b7d31f544f347a29bca71d5247778a5008e00f73
Arthur <arthurcalazans@gmail.com>
2025-01-24 15:02:11 -0300

    integração com React + Vite

--- backend/api_root/urls.py
+++ backend/api_root/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('rest_framework_social_oauth2.urls')),  # Rota para autenticação
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Minha aplicacao React está funcionado !</h1>
+    </>
+  )
+}
+
+export default App
