742c95793b311bf3ebda6492709b3d83d09e3a74
Arthur <arthurcalazans@gmail.com>
2025-01-29 14:22:49 -0300

    AbstractUser

--- backend/gymfrangos/models.py
+++ backend/gymfrangos/models.py
@@ -1,11 +1,19 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
-class User(models.Model):
-    user_name = models.CharField(max_length=150, default='')
-    user_email = models.EmailField(default='')
+class CustomUser(AbstractUser):
+    # Deixa campo 'username' não obrigatório
+    username = models.CharField(max_length=150, null=True, blank=True, unique=True)
+    # Email como identificador único
+    email = models.EmailField(unique=True)
+
+    # Campo de email como o campo de identificação do usuário
+    USERNAME_FIELD = 'email'
+
+    # Campos obrigatórios ao criar um usuário (além de USERNAME_FIELD e password)
+    REQUIRED_FIELDS = []
 
     def __str__(self):
-        return f'Name: {self.user_name} | E-mail: {self.user_email}'
+        return 'Email | {self.email}'
 
 
