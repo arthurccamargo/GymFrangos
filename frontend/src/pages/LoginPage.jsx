df7214b56fc59977c993c19cf77313c0ac111cb8
Arthur <arthurcalazans@gmail.com>
2025-02-03 23:58:30 -0300

    LoginPage comments

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -2,40 +2,45 @@ import { useState } from 'react'
 import { useNavigate } from "react-router-dom"; 
 
 const LoginPage = () => {
-  const navigate = useNavigate(); // Hook para navegar entre páginas
-  const [formData, setFormData] = useState({
+  const navigate = useNavigate(); //Hook do React Router para navegação entre páginas
+  const [formData, setFormData] = useState({ //Hook para gerenciar estado do form
     email: '',
     password: ''
   })
 
-  const [error, setError] = useState('')
+  const [error, setError] = useState('') //estado para mensagens de erro
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    //Quando um formulário é submetido, o comportamento padrão do navegador é recarregar a página
+    e.preventDefault() //impede que recarregue a página, ent, + controle após envio do from
     try {
-      const response = await fetch('http://localhost:8000/login/', {
-        method: 'POST',
+      // await pausa a execução da função até que a Promise seja resolvida e retorna o valor resolvido
+      const response = await fetch('http://localhost:8000/login/', { //onde estamos enviando a requisição
+        method: 'POST', //estamos fazendo uma requisição POST
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json' //corpo da requisição será enviado no formato JSON
         },
-        body: JSON.stringify({
-          email: formData.email,
+        body: JSON.stringify({ //JSON.stringify converte o objeto JavaScript em uma string JSON
+          email: formData.email, //é o corpo da requisição, dados do formulário enviados
           password: formData.password
         })
       })
 
-      const data = await response.json()
+      //Converte a resposta da requisição (que está no formato JSON) em um objeto JavaScript
+      const data = await response.json() //await é usado para esperar que a conversão seja concluída
 
       if (!response.ok) {
         setError(data.message || 'Login falhou. Tente novamente!')
-        return
+        return //para sair da função imediatamente após definir o erro
       }
 
-      // Armazena os tokens no localStorage ou state
+      //Armazena os tokens no localStorage ou state
+      // localStorage é uma forma de armazenamento persistente no navegador, 
+      // o que significa que os dados permanecem lá mesmo após o navegador ser fechado
       localStorage.setItem('access', data.access)
       localStorage.setItem('refresh', data.refresh)
 
-      navigate("/dashboard"); // Redireciona para o Dashboard
+      navigate("/dashboard"); //redireciona para o Dashboard, após um login bem-sucedido
     } catch (error) {
       setError(error.message);
     }
@@ -76,7 +81,7 @@ const LoginPage = () => {
           <input
             type="email"
             placeholder="Email"
-            value={formData.email}
+            value={formData.email} //controle de componente
             onChange={(e) => setFormData({ ...formData, email: e.target.value })}
             style={{
               padding: '0.8rem',
@@ -88,7 +93,7 @@ const LoginPage = () => {
           <input
             type="password"
             placeholder="Senha"
-            value={formData.password}
+            value={formData.password} //controle de componente
             onChange={(e) => setFormData({ ...formData, password: e.target.value })}
             style={{
               padding: '0.8rem',
@@ -115,8 +120,8 @@ const LoginPage = () => {
             border: 'none',
             borderRadius: '5px',
             cursor: 'pointer',
-            fontWeight: 'bold'
-          }} onClick={() => window.location.href = '/register'}>
+            fontWeight: 'bold' //redireciona o usuário para a página /register
+          }} onClick={() => window.location.href = '/register'}> 
             Criar conta
           </button>
         </form>
