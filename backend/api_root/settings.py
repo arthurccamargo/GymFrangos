24efc9f8364c5cf832d2ea79ad5b04369fa8cdad
Arthur <arthurcalazans@gmail.com>
2025-02-04 21:27:26 -0300

    URLs api/login

--- backend/api_root/settings.py
+++ backend/api_root/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*8+7qa#iwd_h=d$jk%%z^zei+cfl)olsvz!6hyt3_c&o2u89&t'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- backend/api_root/urls.py
+++ backend/api_root/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls), # Rota para o painel de administração do Django
     path('auth/', include('rest_framework_social_oauth2.urls')),  # Rota para autenticação social
-    path('', TemplateView.as_view(template_name='index.html')), # Rota para página inicial do site
     path('', include('gymfrangos.urls')),  # Inclui as URLs de gymfrangos
+
+    # Se a URL não for de uma API, serve o index.html do React
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html'), name='react'),
 ]
