dc71591a406739a9156866684b7eafb4602fd32d
Arthur <arthurcalazans@gmail.com>
2025-02-06 11:55:02 -0300

    favicon, font family Google

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,23 +1,39 @@
 import { IoMenu } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
 //import logoImage from '../assets/frango.png'
 
 function Navbar() { 
+  const navigate = useNavigate();
     return (
       <header>
       <div className="flex fixed w-full px-3 md:px-20 py-4 bg-red-600">
         <div className="container flex items-center mx-auto">
           
-          <div className="w-full text-3xl font-bold text-white">GymFrangos</div>
+        <div className="flex items-center">
+            <img 
+              src="/static/assets/favicon.webp" 
+              alt="GymFrangos Logo" 
+              className="w-10 h-10 mr-2 rounded-full"
+            />
+            <div className="text-3xl font-display-poetsen text-white">GymFrangos</div>
+          </div>
+
           <div className="items-end flex-1 text-white font-bold hidden md:flex">
             <nav className="flex-1">
-            <ul className="flex justify-end flex-1">
-              <li className="p-4 hover:text-yellow-200">Features</li>
-              <li className="p-4 hover:text-yellow-200">Pricing</li>
-              <li className="p-4 hover:text-yellow-200">Contact</li>
-              <li className="p-4 hover:text-yellow-200">
-                <span className="border-r border-white"></span>
+            <ul className="flex items-center justify-end text-lg font-display-baloo flex-1">
+              <li className="p-4 hover:text-yellow-200">Características</li>
+              <li className="p-4 hover:text-yellow-200">Pacotes</li>
+              <li className="p-4 hover:text-yellow-200">Contato</li>
+              <li className="p-4 hover:text-yellow-200 mr-3">
+                <span className="border-r border-l-2 border-white"></span>
               </li>
-              <li className="p-4 hover:text-yellow-200">Começar</li>
+              <li>
+                <button
+                  onClick={() => navigate("/login")}
+                  className="px-4 py-2 text-red-700 bg-white rounded-3xl shadow-md transition-transform transform hover:scale-105 hover:bg-gray-100">
+                  Iniciar
+                </button>
+              </li>
             </ul>
             </nav>
           </div>
--- frontend/src/pages/HomePage/HomeMain.jsx
+++ frontend/src/pages/HomePage/HomeMain.jsx
@@ -7,17 +7,17 @@ const HomeMain = () => {
     <main className="flex flex-col items-center justify-center flex-grow text-center text-white bg-red-600 px-6 py-20">
         <img src="/static/assets/frango.png" alt="Logo GymFrangos" className="w-32 md:w-40 lg:w-56"/>
 
-        <h1 className="text-5xl md:text-6xl font-bold mb-6">
+        <h1 className="text-5xl md:text-7xl font-display-titan mb-6">
           Frangos sempre juntos
         </h1>
-        <p className="text-xl md:text-2xl mb-2">
-          Entre nessa comunidade para deixar de ser frango
+        <p className="text-xl md:text-2xl font-display-baloo mb-2">
+          Entre nessa comunidade para deixar de ser FRANGO
         </p>
-        <p className="text-xl md:text-2xl mb-6">Todos frangos em um mesmo lugar</p>
+        <p className="text-xl md:text-2xl font-display-baloo mb-6">Todos frangos em um mesmo lugar</p>
 
         <button
           onClick={() => navigate("/login")}
-          className="px-6 py-3 text-lg font-semibold text-red-700 bg-white rounded-lg shadow-md transition-transform transform hover:scale-105 hover:bg-gray-100"
+          className="px-6 py-3 text-lg font-display-poetsen text-red-700 bg-white rounded-4xl shadow-md transition-transform transform hover:scale-105 hover:bg-gray-100"
         >
           Começar Agora
         </button>
