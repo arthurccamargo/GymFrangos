cc2eb19a50feee2af81f2f3434b8eb9e5482952d
Arthur <arthurcalazans@gmail.com>
2025-02-06 15:55:38 -0300

    add username

--- backend/gymfrangos/views/auth_views.py
+++ backend/gymfrangos/views/auth_views.py
@@ -21,11 +21,15 @@ class RegisterView(generics.CreateAPIView):
     # método create é chamado quando uma requisição POST é feita para a view
     def create(self, request, *args, **kwargs):
         email = request.data.get('email')
+        username = request.data.get('username')
 
         # verifica se o email já existe no banco de dados
         if User.objects.filter(email=email).exists(): 
             return Response(
                 {"message": "This email is already registered"}, status=status.HTTP_400_BAD_REQUEST)
+        # verifica se o username já existe no banco de dados
+        if User.objects.filter(username=username).exists():
+            return Response({"message": "This username is already taken"}, status=status.HTTP_400_BAD_REQUEST)
         
         serializer = self.get_serializer(data=request.data) # instancia o UserSerializer com esses dados
         serializer.is_valid(raise_exception=True) # valida os dados com base nas regras do UserSerializer
@@ -50,3 +54,28 @@ class RegisterView(generics.CreateAPIView):
 class LoginView(TokenObtainPairView): # faz a autenticação do usuário e retorna os tokens JWT
     """ View para login de usuário """
     permission_classes = [AllowAny]  # permite que qualquer pessoa acesse a view de login
+
+    def post(self, request, *args, **kwargs):
+        username_or_email = request.data.get('username_or_email')  # O campo pode ser username ou email
+        password = request.data.get('password')
+
+        if not username_or_email or not password:
+            return Response({"detail": "Username or email and password are required."}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Tenta obter o usuário com base no email se não contiver '@' então busca por username
+        try:
+            user = User.objects.get(email=username_or_email) if '@' in username_or_email else User.objects.get(username=username_or_email)
+        except User.DoesNotExist:
+            return Response({"detail": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+
+        # Verifica se a senha corresponde
+        if not user.check_password(password):
+            return Response({"detail": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+
+        # Gera os tokens JWT
+        refresh = RefreshToken.for_user(user)
+        return Response({
+            "message": "Login successful",
+            "access": str(refresh.access_token),
+            "refresh": str(refresh),
+        }, status=status.HTTP_200_OK)
