282393219bfd45c61ea1e40c5cb37fdc865ff2a8
Arthur <arthurcalazans@gmail.com>
2025-02-06 14:02:49 -0300

    Navbar mobile

--- frontend/src/pages/HomePage/HomeNavbar.jsx
+++ frontend/src/pages/HomePage/HomeNavbar.jsx
@@ -1,44 +1,65 @@
+import PropTypes from 'prop-types';
 import { IoMenu } from "react-icons/io5";
 import StartButton from "../../components/StartButton";
 
-function HomeNavbar() { 
-    return (
-      <header>
-      <div className="flex fixed w-full px-3 md:px-20 py-4 bg-red-600">
+function HomeNavbar({ menuOpen, setMenuOpen }) { 
+  return (
+    <header>
+      <div className="flex fixed w-full px-3 md:px-20 py-4 bg-red-600 z-50">
         <div className="container flex items-center mx-auto">
           
-        <div className="flex items-center">
-            <img 
-              src="/static/assets/favicon.webp" 
-              alt="GymFrangos Logo" 
-              className="w-10 h-10 mr-2 rounded-full"
-            />
-            <div className="text-3xl font-display-poetsen text-white">GymFrangos</div>
+          <div className="flex items-center">
+              <img 
+                src="/static/assets/favicon.webp" 
+                alt="GymFrangos Logo" 
+                className="w-10 h-10 mr-2 rounded-full"
+              />
+              <div className="text-3xl font-display-poetsen text-white">GymFrangos</div>
           </div>
 
           <div className="items-end flex-1 text-white font-bold hidden lg:flex">
             <nav className="flex-1">
-            <ul className="flex items-center justify-end text-lg font-display-baloo flex-1">
-              <li className="p-4 hover:text-yellow-200">Características</li>
-              <li className="p-4 hover:text-yellow-200">Pacotes</li>
-              <li className="p-4 hover:text-yellow-200">Contato</li>
-              <li className="p-4 hover:text-yellow-200 mr-3">
-                <span className="border-r border-l-2 border-white"></span>
-              </li>
-              <li>
-                <StartButton text="Iniciar" route="/login" padding="px-4 py-2" />
-              </li>
-            </ul>
+              <ul className="flex items-center justify-end text-lg font-display-baloo flex-1">
+                <li className="p-4 hover:text-yellow-200">Funcionalidades</li>
+                <li className="p-4 hover:text-yellow-200">Pacotes</li>
+                <li className="p-4 hover:text-yellow-200">Contato</li>
+                <li className="p-4 hover:text-yellow-200 mr-3">
+                  <span className="border-r border-l-2 border-white"></span>
+                </li>
+                <li>
+                  <StartButton text="Iniciar" route="/login" padding="px-4 py-2" />
+                </li>
+              </ul>
             </nav>
           </div>
+
           <div className="flex justify-end flex-1 lg:hidden text-white text-4xl">
-            <IoMenu />
+            <button onClick={() => setMenuOpen(!menuOpen)}>
+                <IoMenu />
+              </button>
           </div>
         </div>
       </div>
-      </header>
-      
+
+      {menuOpen && (
+        <div className="lg:hidden bg-red-600 text-white text-center py-4 w-full absolute top-16 left-0 z-40 shadow-lg">
+          <ul className="text-lg font-display-baloo">
+            <li className="p-4 hover:text-yellow-200">Funcionalidades</li>
+            <li className="p-4 hover:text-yellow-200">Pacotes</li>
+            <li className="p-4 hover:text-yellow-200">Contato</li>
+            <li className="p-4">
+              <StartButton text="Iniciar" route="/login" padding="px-4 py-2" />
+            </li>
+          </ul>
+        </div>
+      )}
+    </header>
     );
+  };
+
+  HomeNavbar.propTypes = {
+    menuOpen: PropTypes.bool.isRequired,
+    setMenuOpen: PropTypes.func.isRequired
   };
   
   export default HomeNavbar;
