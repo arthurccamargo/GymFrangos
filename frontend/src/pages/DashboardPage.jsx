ac64ecc24dec1100d3dd41dd2c4f52c88652c16c
Arthur <arthurcalazans@gmail.com>
2025-02-08 20:45:17 -0300

    Sidebar

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,8 +1,13 @@
+import Sidebar from "../components/Sidebar";
+
 const DashboardPage = () => {
   return (
-    <div className="bg-red-600 min-h-screen flex justify-center items-center text-9xl text-white font-bold text-center p-4"
-    >
-      <h1>Bem-vindo ao Dashboard!</h1>
+    <div className="min-h-screen flex">
+      <Sidebar/>
+      {/* Conteúdo Principal */}
+      <div className="flex-grow bg-red-600 flex justify-center items-center text-9xl text-white font-bold text-center p-4">
+        <h1>Bem-vindo ao Dashboard!</h1>
+      </div>
     </div>
   );
 };
