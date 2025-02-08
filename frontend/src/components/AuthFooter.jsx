79f956044a8c6568c289de0be302820b089cb0eb
Arthur <arthurcalazans@gmail.com>
2025-02-08 00:21:46 -0300

    AuthFooter

---
+++ frontend/src/components/AuthFooter.jsx
@@ -0,0 +1,13 @@
+
+const AuthFooter = () => {
+    return(
+        <footer className="bg-login text-white text-center pt-12 md:pt-18 md:pb-5">
+            < div className="flex justify-center">
+                <hr className="border-t-2 border-gray-500 opacity-25 w-1/2 pb-5 md:pb-10"/>
+            </div>
+            <p>&copy; 2023 GymFrangos. Todos os direitos reservados.</p>
+      </footer>
+    )
+}
+
+export default AuthFooter
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import axios from 'axios';
 import AuthButton from '../components/AuthButton';
 import AuthHeader from '../components/AuthHeader';
+import AuthFooter from '../components/AuthFooter';
 
 const LoginPage = () => {
   const navigate = useNavigate(); // Hook do React Router para navegação entre páginas
@@ -92,7 +93,7 @@ const LoginPage = () => {
                 </svg>
               </div>
             </div>
-
+            
             {/* Botões */}
             <AuthButton 
               type="submit"
@@ -107,13 +108,7 @@ const LoginPage = () => {
           </form>
         </main>
       </div>
-
-      <footer className="bg-login text-white text-center pt-12 md:pt-18 md:pb-5">
-        < div className="flex justify-center">
-          <hr className="border-t-2 border-gray-500 opacity-25 w-1/2 pb-5 md:pb-10"/>
-        </div>
-        <p>&copy; 2023 GymFrangos. Todos os direitos reservados.</p>
-      </footer>
+      <AuthFooter/>
     </div> 
   )
 }
