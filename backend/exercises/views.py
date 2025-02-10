a0d66503f41c6097d9d83e7f0d3886145f042100
Arthur <arthurcalazans@gmail.com>
2025-02-10 19:56:48 -0300

    GET exercises

--- backend/exercises/views.py
+++ backend/exercises/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Exercise
+from .serializers import ExerciseSerializer
 
-# Create your views here.
+class ExerciseListView(generics.ListAPIView): # permite consultas GET para obter múltiplos registros
+    queryset = Exercise.objects.all() # retorna todos os exercícios cadastrados
+    
+    # converte os objetos Exercise para JSON antes de enviá-los para o frontend
+    serializer_class = ExerciseSerializer 
