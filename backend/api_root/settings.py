f285a43b0f860dabaa6597fca5fbce73cf1e9bd4
Arthur <arthurcalazans@gmail.com>
2025-02-11 11:15:16 -0300

    default pagination

--- backend/api_root/settings.py
+++ backend/api_root/settings.py
@@ -154,6 +154,8 @@ REST_FRAMEWORK = {
         'rest_framework_social_oauth2.authentication.SocialAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination', # Paginação
+        'PAGE_SIZE': 20,  # Define quantos exercícios serão retornados por requisição
 }
 
 SIMPLE_JWT = {
