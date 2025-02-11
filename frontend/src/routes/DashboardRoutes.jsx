aad560ee5b9ca03211a1aa2ae17868b077b5b286
Arthur <arthurcalazans@gmail.com>
2025-02-11 15:43:56 -0300

    shadcn-ui config

--- frontend/src/routes/DashboardRoutes.jsx
+++ frontend/src/routes/DashboardRoutes.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import DashboardLayout from "../layouts/DashboardLayout";
 import DashboardHome from "../pages/dashboard/DashboardHome";
-import WorkoutsPage from "../pages/dashboard/WorkoutsPage";
+import ExercisesPage from "../pages/dashboard/ExercisesPage";
 
 const DashboardRoutes = () => {
   return (
@@ -9,7 +9,7 @@ const DashboardRoutes = () => {
       {/* Todas as páginas do dashboard terão a Sidebar */}
       <Route path="/" element={<DashboardLayout />}>  {/* define que todas as rotas dentro do dashboard terão esse layout como base */}
         <Route index element={<DashboardHome />} /> {/* quando o usuário acessar /dashboard sem um sub-path, ele verá a DashboardHome */}
-        <Route path="workouts" element={<WorkoutsPage />} /> {/* sub-rota da rota do DashboardLayout */}
+        <Route path="workouts" element={<ExercisesPage />} /> {/* sub-rota da rota do DashboardLayout */}
       </Route>
     </Routes>
   );
