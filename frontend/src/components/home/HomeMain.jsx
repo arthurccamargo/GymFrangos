d9bc7881b932076641b9010e732f48ef78f028af
Arthur <arthurcalazans@gmail.com>
2025-03-11 16:48:42 -0300

    scalability

--- frontend/src/components/home/HomeMain.jsx
+++ frontend/src/components/home/HomeMain.jsx
@@ -5,9 +5,9 @@ const HomeMain = ({ menuOpen }) => {
   return (
     <main className={`flex flex-col items-center justify-center flex-grow text-center text-white bg-red-600 px-6 py-20 lg:mt-0 ${menuOpen ? "mt-65" : ""}`}>
         <div className="flex">
-          <img src="/static/assets/frango.png" alt="Frango malhando" className="hidden md:w-50 lg:w-56 md:block"/>
-          <img src="/static/assets/frango.png" alt="Frango malhando" className="hidden md:w-50 lg:w-56 md:block"/>
-          <img src="/static/assets/frango.png" alt="Frango malhando" className="w-40 md:w-50 lg:w-56"/>
+          <img src="/assets/frango.png" alt="Frango malhando" className="hidden md:w-50 lg:w-56 md:block"/>
+          <img src="/assets/frango.png" alt="Frango malhando" className="hidden md:w-50 lg:w-56 md:block"/>
+          <img src="/assets/frango.png" alt="Frango malhando" className="w-40 md:w-50 lg:w-56"/>
         </div>
         
         <h1 className="text-5xl md:text-7xl font-display-titan mb-6">
